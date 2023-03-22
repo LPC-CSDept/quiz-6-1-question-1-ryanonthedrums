5dc7eb0644bdb867e38100cfc8f1b2a49bebf309
Ryan Clark <ryanontherun@yahoo.com>
2023-03-22 15:51:20 -0700

    Main

--- q1.hpp
+++ q1.hpp
@@ -2,10 +2,35 @@
 #include <iomanip>
 using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getinput (int &, int &);
+void getinput (float &, float &);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void swapTwoValues (int &, int &);
+void swapTwoValues (float &, float &);
+
+
+void getinput (int &n1, int &n2)
+{
+    cin >> n1 >> n2;
+}
+
+void swapTwoValues (int &n1, int &n2)
+{
+    int tmp;
+    tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+}
+
+void getinput (float &n1, float &n2)
+{
+    cin >> n1 >> n2;
+}
+
+void swapTwoValues (float &n1, float &n2)
+{
+    int tmp;
+    tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+}
